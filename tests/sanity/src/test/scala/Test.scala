2a5b9fdb82c5aba32e54a1e8c783b33f5a4b5407
Liwei Ma <liwei.ma@sifive.com>
2020-09-08 07:54:14 +0000

    matchers

--- tests/sanity/src/test/scala/Test.scala
+++ tests/sanity/src/test/scala/Test.scala
@@ -1,8 +1,8 @@
 import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers._
+import org.scalatest.matchers.should.Matchers
 
-class ExampleSpec extends AnyFlatSpec {
+class ExampleSpec extends AnyFlatSpec with Matchers {
   "List.head" should "be the 0th index of a non-empty List" in {
     val xs = 1 :: 2 :: 3 :: Nil
     xs.head should equal (xs(0))
