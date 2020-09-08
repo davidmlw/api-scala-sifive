6cf30be4fd3156ec5261ea152c54d1c15439e3c6
Liwei Ma <liwei.ma@sifive.com>
2020-09-08 07:41:13 +0000

    adjust versions

--- tests/sanity/src/test/scala/Test.scala
+++ tests/sanity/src/test/scala/Test.scala
@@ -1,4 +1,6 @@
+import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
 
 class ExampleSpec extends AnyFlatSpec {
   "List.head" should "be the 0th index of a non-empty List" in {
